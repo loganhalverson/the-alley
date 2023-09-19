91bcbf0da08af1321821eb787ff99c6edadd3234
Logan <halversonL@cwu.edu>
2023-09-19 16:21:56 -0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			colors: {
+				coffee: '#0C0805',
+				coffeeLight: '#302014',
+				coffeeLighter: '#594C42',
+				bogoBlack: '#0a0a0a',
+			},
+			transform: {},
+			animation: {
+				'bogo-fade': 'fadeIn 1.5s cubic-bezier(.36,.19,.28,.97) forwards',
+				'bogo-fade-slow': 'fadeInSlow 4s cubic-bezier(.36,.19,.28,.97) forwards',
+				'bogo-bg': 'newBgColor 0.25s ease-in-out forwards',
+			},
+			keyframes: {
+				fadeIn: {
+					'0%': { opacity: 0.4 },
+					'100%': { opacity: 1 },
+				},
+				fadeInSlow: {
+					'0%, 50%': { opacity: 0 },
+					'100%': { opacity: 1 },
+				},
+				newBgColor: {
+					from: { backgroundColor: '#0C0805' },
+					to: { backgroundColor: '#302014' },
+				},
+			},
+		},
+	},
+	plugins: ['prettier-plugin-tailwindcss'],
+};
